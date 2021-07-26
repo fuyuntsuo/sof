e73e991f76a07520459a85be46bfc34f20a5f1f5
Keyon Jie <yang.jie@linux.intel.com>
2021-07-26 18:25:46 +0800

    apollolake: make full use of HP SRAM banks
    Implementation of calculating the runtime heap size at run time for
    Intel Apollolake platforms.
    
    Signed-off-by: Keyon Jie <yang.jie@linux.intel.com>

--- src/platform/apollolake/include/platform/lib/memory.h
+++ src/platform/apollolake/include/platform/lib/memory.h
@@ -149,10 +149,8 @@
  * +------------------+-------------------------+-----------------------------+
  * | SOF_FW_START     | text                    |                             |
  * |                  | data                    |                             |
- * |                  | ----------------------- |                             |
+ * |                  |+-----------------------+|                             |
  * |                  ||BSS:                   ||                             |
- * |                  ||-----------------------++-----------------------------+
- * |                  ||Runtime Heap           ||  HEAP_RUNTIME_SIZE          |
  * |                  ||-----------------------++-----------------------------+
  * |                  ||Runtime shared Heap    ||  HEAP_RUNTIME_SHARED_SIZE   |
  * |                  ||-----------------------++-----------------------------+
@@ -169,7 +167,9 @@
  * |                  ||Sec. core Sys Heap     ||  SOF_CORE_S_T_SIZE          |
  * |                  ||Sec. Sys Runtime Heap  ||                             |
  * |                  ||Secondary core Stack   ||                             |
- * |                  | ----------------------- |                             |
+ * |                  ||-----------------------++-----------------------------+
+ * |                  ||Runtime Heap           ||  HEAP_RUNTIME_SIZE          |
+ * |                  |+-----------------------+|                             |
  * +------------------+-------------------------+-----------------------------+
  */
 
@@ -249,6 +249,9 @@
 /* max size for all var-size sections (text/rodata/bss) */
 #define SOF_FW_MAX_SIZE		(HP_SRAM_BASE + HP_SRAM_SIZE - SOF_FW_BASE)
 
+/* TODO: the last 4KB is not usable with QEMU, need to debug it. */
+#define SOF_FW_END		(HP_SRAM_BASE + HP_SRAM_SIZE - 0x1000)
+
 #define SOF_TEXT_START		(SOF_FW_START)
 #define SOF_TEXT_BASE		(SOF_FW_START)
 
@@ -262,45 +265,34 @@
 #define HEAP_SYS_RT_X_COUNT512		4
 #define HEAP_SYS_RT_X_COUNT1024		2
 
-/* Heap section counts base */
+/* Heap section counts base, 64KB in total, for SKL/KBL */
 #define HEAP_COUNT64		128
 #define HEAP_COUNT128		128
 #define HEAP_COUNT256		96
-#define HEAP_COUNT512		12
-#define HEAP_COUNT1024		3
-#define HEAP_COUNT2048		1
-#define HEAP_COUNT4096		0
+#define HEAP_COUNT512		8
+#define HEAP_COUNT1024		4
+#define HEAP_COUNT2048		2
+#define HEAP_COUNT4096		1
 
-#define RT_TIMES	1
-#define RT_SHARED_TIMES	1
+#if HP_SRAM_SIZE >= 0x100000
+/* maximum all SRAM banks used as runtime heap */
+#define RT_COUNT_MAX	(HP_SRAM_SIZE / HEAP_RUNTIME_UNIT_SIZE)
+#else
+/* very limited SRAM on apollolake, less than half of SRAM banks used as runtime heap */
+#define RT_COUNT_MAX	(HP_SRAM_SIZE / (HEAP_RUNTIME_UNIT_SIZE * 2))
+#endif
 
-/* Heap section sizes for module pool */
-#define HEAP_RT_COUNT64			(HEAP_COUNT64 * RT_TIMES)
-#define HEAP_RT_COUNT128		(HEAP_COUNT128 * RT_TIMES)
-#define HEAP_RT_COUNT256		(HEAP_COUNT256 * RT_TIMES)
-#define HEAP_RT_COUNT512		(HEAP_COUNT512 * RT_TIMES)
-#define HEAP_RT_COUNT1024		(HEAP_COUNT1024 * RT_TIMES)
-#define HEAP_RT_COUNT2048		(HEAP_COUNT2048 * RT_TIMES)
-#define HEAP_RT_COUNT4096		(HEAP_COUNT4096 * RT_TIMES)
+#define HEAP_RUNTIME_UNIT_SIZE \
+	(HEAP_COUNT64 * 64 + HEAP_COUNT128 * 128 + \
+	HEAP_COUNT256 * 256 + HEAP_COUNT512 * 512 + \
+	HEAP_COUNT1024 * 1024 + HEAP_COUNT2048 * 2048 + \
+	HEAP_COUNT4096 * 4096)
 
-/* Heap configuration */
-#define HEAP_RUNTIME_SIZE \
-	(HEAP_RT_COUNT64 * 64 + HEAP_RT_COUNT128 * 128 + \
-	HEAP_RT_COUNT256 * 256 + HEAP_RT_COUNT512 * 512 + \
-	HEAP_RT_COUNT1024 * 1024 + HEAP_RT_COUNT2048 * 2048 + \
-	HEAP_RT_COUNT4096 * 4096)
-
-/* Heap section sizes for runtime shared heap */
-#define HEAP_RUNTIME_SHARED_COUNT64	(HEAP_COUNT64 * RT_SHARED_TIMES)
-#define HEAP_RUNTIME_SHARED_COUNT128	(HEAP_COUNT128 * RT_SHARED_TIMES)
-#define HEAP_RUNTIME_SHARED_COUNT256	(HEAP_COUNT256 * RT_SHARED_TIMES)
-#define HEAP_RUNTIME_SHARED_COUNT512	(HEAP_COUNT512 * RT_SHARED_TIMES)
-#define HEAP_RUNTIME_SHARED_COUNT1024	(HEAP_COUNT1024 * RT_SHARED_TIMES)
-
-#define HEAP_RUNTIME_SHARED_SIZE \
-	(HEAP_RUNTIME_SHARED_COUNT64 * 64 + HEAP_RUNTIME_SHARED_COUNT128 * 128 + \
-	HEAP_RUNTIME_SHARED_COUNT256 * 256 + HEAP_RUNTIME_SHARED_COUNT512 * 512 + \
-	HEAP_RUNTIME_SHARED_COUNT1024 * 1024)
+#if CONFIG_CORE_COUNT > 1
+/* very limited SRAM on apollolake, can't afford too much blocks for runtime_shared */
+#define RT_SHARED_COUNT		1
+#define HEAP_RUNTIME_SHARED_SIZE	(HEAP_RUNTIME_UNIT_SIZE * RT_SHARED_COUNT)
+#endif
 
 /* Heap section sizes for system shared heap */
 #define HEAP_SYSTEM_SHARED_SIZE		0x1500
